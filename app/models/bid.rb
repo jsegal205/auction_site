d1a865e7c63966144fc76e3e6d892414cbc46120
Jim Segal <hello@jimsegal.com>
2024-10-01 10:45:37 -0500

    add validation to check if auction is still live

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,4 +3,13 @@ class Bid < ApplicationRecord
   belongs_to :user
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
+  validate :bid_before_auciton_end
+
+  private
+
+  def bid_before_auciton_end
+    if item.end_time.present? && Time.current > item.end_time
+      errors.add(:base, "Cannot place a bid after the auction has ended")
+    end
+  end
 end
